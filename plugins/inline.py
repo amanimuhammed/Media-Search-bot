b5f706422b71040f1bbe654a730c9385a41c8475
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-08-01 12:17:00 +0530

    🤕

--- plugins/inline.py
+++ plugins/inline.py
@@ -84,7 +84,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('More Bots 🤖', url='https://t.me/My_test_botz/47')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
